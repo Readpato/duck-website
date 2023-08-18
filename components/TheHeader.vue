bfa1fc09a0b8f5173751032e48b0eaaf7f68c50c
Patrick Raedler <patoraedler@gmail.com>
2023-08-18 23:16:14 +0200

    refactor(TheHeader): add title and github link

---
+++ components/TheHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const gitHubLink = 'https://github.com/Readpato'
+</script>
+
+<template>
+  <header class="w-full h-12 p-2 border-b border-b-white">
+    <nav class="flex justify-between items-center w-full h-full">
+      <RouterLink to="/" class="font-bold">
+        Patrick Raedler
+      </RouterLink>
+      <ul>
+        <li>
+          <a :href="gitHubLink" class="i-carbon-logo-github w-7 h-7 block" target="_blank" />
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
