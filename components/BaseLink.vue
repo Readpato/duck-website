577d49af4dec413249cc3c9ac717f92abf8dd6e0
Patrick Raedler <patoraedler@gmail.com>
2023-08-19 16:26:02 +0200

    feat(BaseLink): create custom anchor component

---
+++ components/BaseLink.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  icon?: string
+  text?: string
+  link: string
+}>()
+</script>
+
+<template>
+  <a :href="link" target="_blank">
+    <div v-if="icon" :class="`i-carbon-${icon}`" class="w-7 h-7 block" />
+    {{ text }}
+  </a>
+</template>
+
+<style scoped lang="scss">
+a {
+  color: $c-link;
+  opacity: 0.6;
+  transition: opacity 0.2s ease;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
