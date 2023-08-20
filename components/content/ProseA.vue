c7e7aeffd38c8311f1de1dcdd27ca6c21fd2f6bd
Patrick Raedler <patoraedler@gmail.com>
2023-08-20 13:11:22 +0200

    feat(ProseA): modify ProseA component

---
+++ components/content/ProseA.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  href: string
+  target?: string
+}>(), {
+  target: '_blank',
+})
+</script>
+
+<template>
+  <NuxtLink :href="href" :target="target">
+    <slot />
+  </NuxtLink>
+</template>
+
+<style scoped lang="scss">
+a {
+  border-bottom: 1px solid rgba($c-border, .3);
+  color: $c-white;
+  opacity: 1;
+  text-decoration: none;
+  transition: border-bottom 0.2s ease;
+
+  &:hover {
+    border-bottom: 1px solid rgba($c-border, 1);
+  }
+}
+</style>
