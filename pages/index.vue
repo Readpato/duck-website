1db29fa58f40fb47ae5d0294638af6a13b6a590c
Patrick Raedler <patoraedler@gmail.com>
2023-08-20 13:11:49 +0200

    feat(index.vue): add main content MD file

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full max-w-160 mx-auto px-7 py-10">
+    <h1 class="mt-2 mb-6 text-3xl font-bold text-white">
+      Hello there!
+    </h1>
+    <article>
+      <ContentDoc />
+    </article>
+  </div>
+</template>
