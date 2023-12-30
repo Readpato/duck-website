7418f6ddb4fa26b587dc50592574a49bb0b1232f
Patrick Raedler <patoraedler@gmail.com>
2023-12-30 17:42:46 +0100

    chore(deps): update antfu eslint config

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignorePatterns: ['*.code-workspace'],
+  rules: {
+    'antfu/top-level-function': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+  },
+})
