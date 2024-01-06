456633d7347d8ce851ec65e019b1d1019a7a1e24
Patrick Raedler <patoraedler@gmail.com>
2024-01-06 12:33:31 +0100

    feat(blog/index): create placeholder page, create query for articles

---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data } = await useAsyncData('home', () => queryContent('blog')
+  .where({ date: { $exists: true } })
+  .sort({ date: -1 })
+  .find())
+</script>
+
+<template>
+  <div class="rp-container">
+    <h1 class="rp-title">
+      Blog
+    </h1>
+    <pre>
+      {{ data }}
+    </pre>
+  </div>
+</template>
