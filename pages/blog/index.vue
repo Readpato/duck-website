b1873f66dde0fa5866fa5e1aa8e050ec0a21d832
Patrick Raedler <patoraedler@gmail.com>
2024-01-06 14:30:21 +0100

    refactor(blog/index): add todos, create initial placeholders

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,6 +3,9 @@ const { data } = await useAsyncData('home', () => queryContent('blog')
   .where({ date: { $exists: true } })
   .sort({ date: -1 })
   .find())
+
+// TODO: Add proper loading state for fallback template
+// TODO: Extend types properly https://content.nuxt.com/usage/typescript#type-augmentation
 </script>
 
 <template>
@@ -10,8 +13,19 @@ const { data } = await useAsyncData('home', () => queryContent('blog')
     <h1 class="rp-title">
       Blog
     </h1>
-    <pre>
-      {{ data }}
-    </pre>
+    <ClientOnly>
+      <ArticleCard
+        v-for="{ title, description, date, _path, _id } in data"
+        :key="_id"
+        :title="title as string"
+        :description="description"
+        :date="date"
+        :path="_path as string"
+        mb2
+      />
+      <template #fallback>
+        <span>Loading...</span>
+      </template>
+    </ClientOnly>
   </div>
 </template>
